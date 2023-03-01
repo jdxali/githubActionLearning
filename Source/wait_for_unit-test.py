b9e4eb09691a5a7bb9bfdac6441489fc1bfbc47b
Ali Shan <ali.shan@jedox.com>
2023-03-01 13:49:26 +0100

    add token

--- Source/wait_for_unit-test.py
+++ Source/wait_for_unit-test.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
         print(resp.status)
         if resp.status == 200:
             body = resp.read()
-            status = json.loads(body)[0]["conclusion"]
+            status = json.loads(body)["total_count"]
             print ("job status")
             print (body)
             print(status)
