2e1ab8c8fca5929bd132764fed561a139fed36c0
Ali Shan <ali.shan@jedox.com>
2023-03-01 16:19:46 +0100

    test script

---
+++ Source/wait.py
@@ -0,0 +1,7 @@
+import os
+import ssl
+import time
+
+
+if __name__ == "__main__":
+    time.sleep(200)
--- Source/wait_for_unit-test.py
+++ Source/wait_for_unit-test.py
@@ -17,28 +17,20 @@ if __name__ == "__main__":
         "Authorization": "token " + MY_TOKEN,
         "User-Agent": "jedox-ag",
     }
-
-    job_names = [
-       "build",
-       "unit-test",
-    ]
-    
+ 
     check_status = True
     while check_status:
         conn = http.client.HTTPSConnection("api.github.com")
         conn.request("GET", "/repos/jdxali/githubActionLearning/actions/runs/" +GITHUB_RUN_ID +"/jobs", None, headers)
         resp = conn.getresponse()
         body = resp.read()
-        print(body)
-        print(resp.status)
         if resp.status == 200:
-            body = resp.read()
-            status = json.loads(body)["total_count"]
-            print ("job status")
-            print (body)
-            print(status)
-            if status == "success":
-                check_status =False
-            time.sleep(5)
-
-_
+            jobs = json.loads(body)["jobs"]
+            for job in jobs:
+                if job["name"] == "second job":
+                   print (job["status"])
+                   if job["conclusion"] == "success":
+                      check_status =False
+                   if job["conclusion"] == "failure":
+                       raise Exception("Unit-test job failed")
+        time.sleep(5)
