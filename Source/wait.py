20d80ff7135a9ae21d810f2227080c4776ea6db8
Ali Shan <ali.shan@jedox.com>
2023-03-16 15:24:28 +0100

    add script to comment

--- Source/wait.py
+++ Source/wait.py
@@ -1,8 +1,29 @@
+import http.client
+import json
 import os
 import ssl
 import time
+import sys
 
 
 if __name__ == "__main__":
-    time.sleep(60)
-    os._exit(1)
+    #GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
+    MY_TOKEN = os.environ["MY_TOKEN"]
+    GITHUB_REF = os.environ["GITHUB_REF"]
+    GITHUB_JOB = os.environ["GITHUB_JOB"]
+    GITHUB_RUN_ID= os.environ["GITHUB_RUN_ID"]
+
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": "token " + MY_TOKEN,
+        "User-Agent": "test_ag",
+        "body" : "Great stuff!"
+    }
+ 
+    conn = http.client.HTTPSConnection("api.github.com")
+    conn.request("POST", "/repos/jdxali/githubActionLearning/pulls/5/comments", None, headers)
+    resp = conn.getresponse()
+    body = resp.read()
+    print(body)
+       
+    
