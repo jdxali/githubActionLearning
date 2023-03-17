61051cf07f6107278c2090f25924d1fc54c704d7
Ali Shan <ali.shan@jedox.com>
2023-03-17 11:02:58 +0100

    print

--- Source/wait.py
+++ Source/wait.py
@@ -1,8 +1,37 @@
+import http.client
+import json
 import os
 import ssl
 import time
+import sys
 
 
 if __name__ == "__main__":
-    time.sleep(60)
-    os._exit(1)
+    #GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
+    MY_TOKEN = os.environ["MY_TOKEN"]
+    GITHUB_REF = os.environ["GITHUB_REF"]
+    GITHUB_JOB = os.environ["GITHUB_JOB"]
+    GITHUB_RUN_ID= os.environ["GITHUB_RUN_ID"]
+    GIT_EVENT_NAME = os.environ["GITHUB_EVENT_NAME"]
+
+    print (GITHUB_REF)
+    print(GIT_EVENT_NAME)
+    
+
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": "token " + MY_TOKEN,
+        "User-Agent": "jedox_ag",
+    }
+
+    data = { "body" : "new comment" }
+    json_data = json.dumps(data)
+ 
+    conn = http.client.HTTPSConnection("api.github.com")
+    conn.request("POST", "/repos/jdxali/githubActionLearning/issues/5/comments", json_data, headers) 
+    resp = conn.getresponse()
+    body = resp.read()
+    print(resp.status)
+    print(body)
+       
+    
