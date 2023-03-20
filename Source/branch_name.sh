15dc45587ca903b2ef59291d9c21b16f4a84bfde
Ali Shan <ali.shan@jedox.com>
2023-03-20 13:17:46 +0100

    pass from workflow

--- Source/branch_name.sh
+++ Source/branch_name.sh
@@ -4,9 +4,8 @@ function get_ticket_number() {
 
 BRANCH_NAME=$GITHUB_REF_HEAD
 readarray -d "-" -t arr <<< "$BRANCH_NAME"
-TICKET_NUM="A-TOLP-${arr[1]}"
-echo "TICKET_NUM=$TICKET_NUM" >> $GITHUB_ENV
-echo $TICKET_NUM
+AHA_TICKET_NUM="A-TOLP-${arr[1]}"
+echo $AHA_TICKET_NUM
 }
 
 get_ticket_number
