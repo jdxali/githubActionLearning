9cbf4ab2fe1dab45e2aa2b14426d6609cdaa6e27
Ali Shan <ali.shan@jedox.com>
2023-03-20 13:09:42 +0100

    get token num

--- Source/branch_name.sh
+++ Source/branch_name.sh
@@ -4,7 +4,8 @@ function get_ticket_number() {
 
 BRANCH_NAME=$GITHUB_REF_HEAD
 readarray -d "-" -t arr <<< "$BRANCH_NAME"
-export TICKET_NUM="A-TOLP-${arr[1]}"
+TICKET_NUM="A-TOLP-${arr[1]}"
+echo "TICKET_NUM=$TICKET_NUM" >> $GITHUB_ENV
 echo $TICKET_NUM
 }
 
