4840900802c68bd088bce2cfb8530ba1784bb0ce
Ali Shan <ali.shan@jedox.com>
2023-03-21 09:21:40 +0100

    newpr

--- Source/branch_name.sh
+++ Source/branch_name.sh
@@ -3,8 +3,10 @@
 function get_ticket_number() {
 BRANCH_NAME=$GITHUB_HEAD_REF
 readarray -d "-" -t arr <<< "$BRANCH_NAME"
-AHA_TICKET_NUM="A-TOLP-${arr[1]}"
-echo "AHA_TICKET_NUM=$AHA_TICKET_NUM" >> $GITHUB_ENV
+AHA_TICKET_NUM="AHA Ticket : A-TOLP-${arr[1]}\n"
+PR_TITLE="PR TITLE : ${github.event.pull_request.title}\n"
+BRANCH_TITLE="BRANCH : ${GITHUB_HEAD_REF}\n"
+echo "AHA_TICKET_NUM=$AHA_TICKET_NUM $PR_TITLE $BRANCH_TITLE" >> $GITHUB_ENV
 echo $AHA_TICKET_NUM
 }
 
